a5c2c594dcd94619433d15f5a371adfdcfce914e
masamasace <shigamasataka@gmail.com>
2025-03-24 14:15:36 +0900

    remove unnecessary files: delete .DS_Store, check_decode.py, check_soil_class.py, and check_soil_class_xml_read.py

--- misc/check_decode.py
+++
@@ -1,6 +0,0 @@
-byte_str = "0909093c904692b25f904692b296bc3e88c390c28a44816092838a8c816088c392838a8c3c2f904692b25f904692b296bc3e0d0a"
-
-# Decode the byte string
-byte_str = bytes.fromhex(byte_str)
-print(byte_str)
-print(byte_str.decode("shift-jis"))
--- misc/check_soil_class_xml_read.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-import lxml.etree as ET
-
-file_path = r"misc/example.xml"
-
-parser = ET.XMLParser(encoding="shift-jis")
-
-tree = ET.parse(file_path, parser=parser)
